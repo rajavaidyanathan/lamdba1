b7a418d80d682e7d2442170a11680badf30b87a0
rajavaidyanathan <v_raja_v@yahoo.com>
2018-09-01 23:06:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("Asia/Mumbai");
+    currentTime.setTimezone("Europe/London");
     callback(null, {
         statusCode: '200',
         body: 'The time in Bangalore is: ' + currentTime.toString(),
